745ddaf06d21058aaedcf6a089ff6e72e91ea603
ramonornelas <ornelas@pythian.com>
2023-10-25 22:01:29 -0600

    first commit

---
+++ function_app.py
@@ -0,0 +1,45 @@
+import azure.functions as func
+import json
+from modules import check_dates
+from modules import get_cookie_from_file
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="endpoint")
+def endpoint(req:func.HttpRequest) -> func.HttpResponse:
+
+    city_id = int(req.params.get('cityId'))
+    cookie = get_cookie_from_file()
+    target_date = '2024-11-27'
+
+    city_names = {
+        65: "Ciudad Juarez",
+        66: "Guadalajara",
+        67: "Hermosillo",
+        68: "Matamoros",
+        69: "Merida",
+        70: "Mexico City",
+        71: "Monterrey",
+        72: "Nogales",
+        73: "Nuevo Laredo",
+        74: "Tijuana",
+    }
+
+    city_name = city_names.get(city_id, "Invalid case")
+    check_dates_result = check_dates(city_id, city_name, target_date, cookie)
+    response = {
+        f"{city_name}": f"{check_dates_result}"
+    }
+    
+    headers = {
+        "Access-Control-Allow-Origin": "*",  # Specify the allowed origin or use "*" for all
+        "Access-Control-Allow-Methods": "GET, POST",  # Specify allowed HTTP methods
+        "Access-Control-Allow-Headers": "Content-Type",  # Specify allowed headers
+    }
+    
+    return func.HttpResponse(
+        body=json.dumps(response),
+        mimetype="application/json",
+        status_code=200,
+        headers=headers
+    )
---
+++ modules.py
@@ -0,0 +1,134 @@
+import requests
+# import sys
+# import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import datetime
+# import requests_mock
+
+
+def print_timestamp(text):
+	print(current_timestamp() + ' // ' + text)
+
+def print_indent(text):
+	print('	' + text)
+
+def current_timestamp():
+	return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+def send_email(subject, message):
+
+	# Set up the SMTP server
+	smtp_server = "smtp.gmail.com"
+	smtp_port = 587
+	smtp_username = "ornelas.carlos@gmail.com"
+	smtp_password = "xstqhkkvrabtalxp"
+
+	# Create the email message
+	sender = "ornelas.carlos@gmail.com"
+	# receiver = "ornelas.carlos@gmail.com, nokktarehezth@gmail.com"
+	receiver = "ornelas.carlos@gmail.com"
+
+	msg = MIMEMultipart()
+	msg["From"] = sender
+	msg["To"] = receiver
+	msg["Subject"] = subject
+	msg.attach(MIMEText(message, "plain"))
+
+	# Connect to the SMTP server and send the email
+	try:
+		with smtplib.SMTP(smtp_server, smtp_port) as server:
+			server.starttls()
+			server.login(smtp_username, smtp_password)
+			server.send_message(msg)
+		print_indent("Email sent successfully!")
+	except Exception as e:
+		print_indent("An error occurred: " + str(e))
+
+def get_cookie_from_file():
+	# Get the cookie value from a text file
+	file_path = "visa_data.txt"
+
+	# Open the text file in read mode
+	file = open(file_path, "r")
+
+	# Read the file line by line
+	lines = file.readlines()
+
+	for index, line in enumerate(lines):
+		if line.startswith("Cookie:"):
+			cookie = lines[index + 1]
+			# Check if the file has more lines after the cookie value to remove the 'New line' character
+			if len(lines) > index + 2:
+				# Remove the last character
+				cookie = cookie[:-1]
+
+	# Close the file
+	file.close()
+
+	return cookie
+
+def check_dates(city_code, city_name, target_date, cookie):
+
+	# Get data from the API
+	url = "https://ais.usvisa-info.com/es-mx/niv/schedule/52250562/appointment/days/" + str(city_code) + ".json?appointments[expedite]=false"
+
+	headers = {
+		"Cookie": cookie,
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+		"X-Requested-With": "XMLHttpRequest"
+	}
+
+	# Send a GET request to the endpoint
+	response = requests.get(url, headers=headers)
+
+	"""
+	# Create a requests_mock instance
+	with requests_mock.Mocker() as mocker:
+		# Define a mock response for a specific API endpoint
+		dates_empty = False
+		if dates_empty:
+			mocker.get('https://api.example.com/endpoint', json=[])
+		else:
+			mocker.get('https://api.example.com/endpoint', json=[{"date": "2026-01-01"},{"date": "2025-02-01"}])
+
+		# Make a request to the mocked API endpoint
+		response = requests.get('https://api.example.com/endpoint')
+	
+	"""
+
+	# Check the response status code
+	if response.status_code == 200:
+		# Parse the response content as JSON
+		try:
+			data = response.json()
+		except:
+			print_indent('Incorrect response format')
+			subject = "Visa - Formato de respuesta incorrecto"
+			message = "https://ais.usvisa-info.com/es-mx/niv/schedule/52250562/appointment"
+			send_email(subject, message)
+			return "Error"
+
+		# Check if there is an available date for appointment
+		if len(data) != 0:
+			available_date = data[0]["date"]
+			if available_date < target_date:
+				subject = "Visa - Fecha disponible!!! (" + city_name + " / " + available_date + ")"
+				message = "https://ais.usvisa-info.com/es-mx/niv/schedule/52250562/appointment"
+				send_email(subject, message)
+				return 'AVAILABLE DATE!!! ' + str(available_date)
+			else:
+				return 'No dates before target date (' + str(target_date) + '). Available dates start on: ' + str(available_date)
+		else:
+			return 'No dates available.'
+
+	else:
+		if response.status_code == 401:
+			subject = "Visa - Terminó la sesión"
+			message = "https://ais.usvisa-info.com/es-mx/niv/schedule/52250562/appointment"
+		else:
+			subject = "Visa - Se generó un error"
+			message = "https://ais.usvisa-info.com/es-mx/niv/schedule/52250562/appointment"
+		send_email(subject, message)
+		return "An error occurred. Status code: " + str(response.status_code)
