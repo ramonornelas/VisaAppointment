0dc6629a3b518d71fc57dbdfa04d6521159260d8
Nokk <ornelas.carlos@gmail.com>
2023-11-05 09:13:51 -0600

    Readded mocked data.

--- function_app.py
+++ function_app.py
@@ -9,10 +9,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def endpoint(req:func.HttpRequest) -> func.HttpResponse:
 
     city_id = int(req.params.get('cityId'))
-    user = req.params.get('user')
-    # computer_name = req.params.get('computerName')
-    # mocked_data_count = int(req.params.get('mockedDataCount'))
-    # start_date = req.params.get('startDate')
+    computer_name = req.params.get('computerName')
+    mocked_data_count = int(req.params.get('mockedDataCount'))
+    start_date = req.params.get('startDate')
 
     cookie = get_cookie_from_file()
     target_date = '2024-10-29'
@@ -31,7 +30,7 @@ def endpoint(req:func.HttpRequest) -> func.HttpResponse:
     }
 
     city_name = city_names.get(city_id, "Invalid case")
-    check_dates_result = check_dates(city_id, city_name, target_date, cookie, user)
+    check_dates_result = check_dates(city_id, city_name, target_date, cookie, computer_name, mocked_data_count, start_date)
     response = {
         f"result": f"{city_name + ': ' + check_dates_result}"
     }
